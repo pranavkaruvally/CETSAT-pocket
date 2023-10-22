515513d68075e120ca1766f26e102ba001e54938
Pranav P <pranavsdreams@gmail.com>
2023-10-22 17:11:34 +0530

    Created double ended queue

---
+++ src/data_structures/deque.c
@@ -0,0 +1,122 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#define SIZE_LIMIT ((1 << 16) - 1)
+
+struct deque {
+  bool queue[SIZE_LIMIT];
+  int16_t size;
+  int16_t front;
+  int16_t rear;
+
+  void (*insert_front)(struct deque*, int);
+  void (*insert_rear)(struct deque*, int);
+  void (*delete_front)(struct deque*);
+  void (*delete_rear)(struct deque*);
+  bool (*is_full)(struct deque*);
+  bool (*is_empty)(struct deque*);
+  bool (*get_front)(struct deque*);
+  bool (*get_rear)(struct deque*);
+};
+
+void insert_front(struct deque*, int);
+void insert_rear(struct deque*, int);
+void delete_front(struct deque*);
+void delete_rear(struct deque*);
+bool is_full(struct deque*);
+bool is_empty(struct deque*);
+bool get_front(struct deque*);
+bool get_rear(struct deque*);
+
+struct deque initialise_deque() {
+  struct deque *dq = malloc(sizeof(struct deque));
+  dq->size = 0;
+  dq->front = dq->rear = -1;
+  dq->insert_front = insert_front;
+  dq->insert_rear = insert_rear;
+  dq->delete_front = delete_front;
+  dq->delete_rear = delete_rear;
+  dq->is_full = is_full;
+  dq->is_empty = is_empty;
+  dq->get_front = get_front;
+  dq->get_rear = get_rear;
+}
+
+bool is_full(struct deque* dq) {
+  return ((dq->front == 0 && dq->rear == dq->size - 1)
+              || dq->front == dq->rear + 1);
+}
+
+bool is_empty(struct deque* dq) { return dq->front == -1; }
+
+void insert_front(struct deque* dq, int key) {
+  if (dq->is_full(dq)) {
+    return;
+  }
+
+  if (dq->front == -1) {
+    dq->front = 0;
+    dq->rear = 0;
+  } else if (dq->front == 0) {
+    dq->front = dq->size - 1;
+  } else {
+    dq->front--;
+  }
+  dq->queue[dq->front] = key;
+}
+
+void insert_rear(struct deque* dq, int key) {
+  if (dq->is_full(dq)) {
+    return;
+  }
+  if (dq->front == -1) {
+    dq->front = 0;
+    dq->rear = 0;
+  } else if (dq->rear == dq->size - 1) {
+    dq->rear = 0;
+  } else {
+    dq->rear++;
+  }
+
+  dq->queue[dq->rear] = key;
+}
+
+void delete_front(struct deque* dq) {
+  if (dq->is_empty(dq)) {
+    return;
+  }
+
+  if (dq->front == dq->rear) {
+    dq->front = -1;
+    dq->rear = -1;
+  }
+  else
+      if (dq->front == dq->size - 1)
+      dq->front = 0;
+ 
+  else 
+      dq->front++;
+}
+
+void delete_rear(struct deque* dq) {
+  if (dq->is_empty(dq)) {
+      return;
+  }
+  
+  if (dq->front == dq->rear) {
+      dq->front = -1;
+      dq->rear = -1;
+  }
+  else if (dq->rear == 0)
+      dq->rear = dq->size - 1;
+  else
+      dq->rear = dq->rear - 1;
+}
+
+bool get_front(struct deque* dq) {
+  return dq->queue[dq->front];
+}
+
+bool get_rear(struct deque* dq) {
+  return dq->queue[dq->rear];
+}
